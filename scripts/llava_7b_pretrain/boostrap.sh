4081687fdf97fa3ae57bef8219d31fdb7a9ff473
wangshichao10 <wangshichao10@meituan.com>
2023-07-21 10:27:33 +0800

    reorganize pretrain script

--- scripts/llava_7b_pretrain/boostrap.sh
+++ scripts/llava_7b_pretrain/boostrap.sh
@@ -2,7 +2,7 @@
 MODEL_HUB_HOME=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-nlpml/wangshichao10/model_hub
 MLLM_DATA_HOME=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-nlpml/wangshichao10/mllm/data
 
-torchrun --nnodes=1 --nproc_per_node=4 --master_port=25001 \
+torchrun --nnodes=1 --nproc_per_node=8 --master_port=25001 \
     llava/train/train_mem.py \
     --model_name_or_path $MODEL_HUB_HOME/huggingface/lmsys/vicuna-7b-v1.3 \
     --version v1 \
@@ -13,11 +13,11 @@ torchrun --nnodes=1 --nproc_per_node=4 --master_port=25001 \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end \
     --bf16 False \
-    --output_dir $MODEL_HUB_HOME/llava-7b-v1-pretrained \
+    --output_dir $MODEL_HUB_HOME/llava-7b-v1-pretrained_test \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 128 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 100 \
